cef09e289409d504e3546804338f5bc7a7869ad1
Peter Ungvari <ungvari.mail@gmail.com>
2016-02-12 20:04:20 +0100

    recording audio streams in PCM format and taking screenshots

---
+++ WiDAC/WiDACForm.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace WiDAC
+{
+    partial class WiDACForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.outputDeviceComboBox = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.bitRateComboBox = new System.Windows.Forms.ComboBox();
+            this.rateComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.inputDeviceComboBox = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.videoBitrateComboBox = new System.Windows.Forms.ComboBox();
+            this.frameRateComboBox = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.screenComboBox = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.captureCheckBox = new System.Windows.Forms.CheckBox();
+            this.previewPanel = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Output device";
+            // 
+            // outputDeviceComboBox
+            // 
+            this.outputDeviceComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.outputDeviceComboBox.FormattingEnabled = true;
+            this.outputDeviceComboBox.Location = new System.Drawing.Point(98, 19);
+            this.outputDeviceComboBox.Name = "outputDeviceComboBox";
+            this.outputDeviceComboBox.Size = new System.Drawing.Size(239, 21);
+            this.outputDeviceComboBox.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.bitRateComboBox);
+            this.groupBox1.Controls.Add(this.rateComboBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 97);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(351, 79);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Audio Capture Settings";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(55, 49);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Bitrate";
+            // 
+            // bitRateComboBox
+            // 
+            this.bitRateComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.bitRateComboBox.FormattingEnabled = true;
+            this.bitRateComboBox.Location = new System.Drawing.Point(98, 46);
+            this.bitRateComboBox.Name = "bitRateComboBox";
+            this.bitRateComboBox.Size = new System.Drawing.Size(239, 21);
+            this.bitRateComboBox.TabIndex = 8;
+            // 
+            // rateComboBox
+            // 
+            this.rateComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.rateComboBox.FormattingEnabled = true;
+            this.rateComboBox.Location = new System.Drawing.Point(98, 19);
+            this.rateComboBox.Name = "rateComboBox";
+            this.rateComboBox.Size = new System.Drawing.Size(239, 21);
+            this.rateComboBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Sample rate";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.inputDeviceComboBox);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.outputDeviceComboBox);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(351, 79);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Audio Capture Devices";
+            // 
+            // inputDeviceComboBox
+            // 
+            this.inputDeviceComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.inputDeviceComboBox.FormattingEnabled = true;
+            this.inputDeviceComboBox.Location = new System.Drawing.Point(98, 46);
+            this.inputDeviceComboBox.Name = "inputDeviceComboBox";
+            this.inputDeviceComboBox.Size = new System.Drawing.Size(239, 21);
+            this.inputDeviceComboBox.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 49);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Input device";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.videoBitrateComboBox);
+            this.groupBox3.Controls.Add(this.frameRateComboBox);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Controls.Add(this.screenComboBox);
+            this.groupBox3.Controls.Add(this.label10);
+            this.groupBox3.Location = new System.Drawing.Point(12, 182);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(351, 107);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Video Capture Settings";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(55, 76);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Bitrate";
+            // 
+            // videoBitrateComboBox
+            // 
+            this.videoBitrateComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.videoBitrateComboBox.FormattingEnabled = true;
+            this.videoBitrateComboBox.Location = new System.Drawing.Point(98, 73);
+            this.videoBitrateComboBox.Name = "videoBitrateComboBox";
+            this.videoBitrateComboBox.Size = new System.Drawing.Size(239, 21);
+            this.videoBitrateComboBox.TabIndex = 8;
+            // 
+            // frameRateComboBox
+            // 
+            this.frameRateComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.frameRateComboBox.FormattingEnabled = true;
+            this.frameRateComboBox.Location = new System.Drawing.Point(98, 46);
+            this.frameRateComboBox.Name = "frameRateComboBox";
+            this.frameRateComboBox.Size = new System.Drawing.Size(239, 21);
+            this.frameRateComboBox.TabIndex = 5;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(35, 49);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(57, 13);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "Frame rate";
+            // 
+            // screenComboBox
+            // 
+            this.screenComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.screenComboBox.FormattingEnabled = true;
+            this.screenComboBox.Location = new System.Drawing.Point(98, 19);
+            this.screenComboBox.Name = "screenComboBox";
+            this.screenComboBox.Size = new System.Drawing.Size(239, 21);
+            this.screenComboBox.TabIndex = 3;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(51, 22);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "Screen";
+            // 
+            // captureCheckBox
+            // 
+            this.captureCheckBox.Appearance = System.Windows.Forms.Appearance.Button;
+            this.captureCheckBox.Location = new System.Drawing.Point(382, 31);
+            this.captureCheckBox.Name = "captureCheckBox";
+            this.captureCheckBox.Size = new System.Drawing.Size(140, 133);
+            this.captureCheckBox.TabIndex = 11;
+            this.captureCheckBox.Text = "Capture";
+            this.captureCheckBox.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.captureCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // previewPanel
+            // 
+            this.previewPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.previewPanel.Location = new System.Drawing.Point(382, 189);
+            this.previewPanel.Name = "previewPanel";
+            this.previewPanel.Size = new System.Drawing.Size(140, 100);
+            this.previewPanel.TabIndex = 12;
+            // 
+            // WiDACForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 301);
+            this.Controls.Add(this.previewPanel);
+            this.Controls.Add(this.captureCheckBox);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "WiDACForm";
+            this.Text = "WiDAC";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox outputDeviceComboBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox rateComboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox bitRateComboBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox inputDeviceComboBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox videoBitrateComboBox;
+        private System.Windows.Forms.ComboBox frameRateComboBox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox screenComboBox;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.CheckBox captureCheckBox;
+        private System.Windows.Forms.Panel previewPanel;
+    }
+}
+
---
+++ WiDAC/WiDACForm.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using CSCore;
+using CSCore.SoundIn;
+using CSCore.CoreAudioAPI;
+using System.Threading;
+using CSCore.Codecs.WAV;
+using System.IO;
+
+namespace WiDAC
+{
+    public partial class WiDACForm : Form
+    {
+        private WasapiCapture outputCapture;
+        private WasapiCapture inputCapture;
+        private WaveWriter outputWaveWriter;
+        private WaveWriter inputWaveWriter;
+        private bool recording;
+        private Stopwatch stopwatch = Stopwatch.StartNew();
+        private long frameCount;
+        System.Windows.Forms.Timer recordingTimer = new System.Windows.Forms.Timer();
+        System.Windows.Forms.Timer previewTimer = new System.Windows.Forms.Timer();
+
+        public WiDACForm()
+        {
+            InitializeComponent();
+            InitializeListValues();
+            LoadDevices(DataFlow.Capture, inputDeviceComboBox);
+            LoadDevices(DataFlow.Render, outputDeviceComboBox);
+
+            previewPanel.BackgroundImageLayout = ImageLayout.Zoom;
+
+            recordingTimer.Interval = 40;
+
+            previewTimer.Interval = 1000;
+            previewTimer.Start();
+
+            recordingTimer.Tick += RecordingTimerTick;
+            previewTimer.Tick += previewTimer_Tick;
+            captureCheckBox.Click += captureCheckBox_Click;
+
+        }
+
+        void previewTimer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                Bitmap previous = (Bitmap)previewPanel.BackgroundImage;
+                Bitmap bmpScreenshot = CreateScreenshot();
+                previewPanel.BackgroundImage = bmpScreenshot;
+
+                if (previous != null)
+                {
+                    previous.Dispose();
+                }
+
+            }
+            catch (ArgumentException ex)
+            {
+                Trace.TraceError("ArgumentException on preview", ex.StackTrace);
+            }
+
+        }
+
+        void captureCheckBox_Click(object sender, EventArgs e)
+        {
+            if (!recording)
+            {
+                Start();
+            }
+            else
+            {
+                Stop();
+            }
+        }
+
+        private void RecordingTimerTick(object sender, EventArgs e)
+        {
+            if (!recording)
+            {
+                return;
+            }
+
+            if (frameCount < stopwatch.ElapsedMilliseconds / 1000.0 * (int)frameRateComboBox.SelectedValue)
+            {
+                try
+                {
+                    ++frameCount;
+
+                    Bitmap bmpScreenshot = CreateScreenshot();
+                    // Save the screenshot to the specified path that the user has chosen.
+                    bmpScreenshot.Save(string.Format("Screenshot{0}.png", frameCount), ImageFormat.Png);
+                    bmpScreenshot.Dispose();
+                }
+                catch (ArgumentException ex)
+                {
+                    Trace.TraceError("ArgumentException on capture", ex.StackTrace);
+                }
+
+            }
+        }
+
+        private Bitmap CreateScreenshot()
+        {
+            //Create a new bitmap.
+            Screen screen = screenComboBox.SelectedValue as Screen;
+            Bitmap bmpScreenshot = new Bitmap(screen.Bounds.Width, screen.Bounds.Height, PixelFormat.Format32bppArgb);
+
+            // Create a graphics object from the bitmap.
+            Graphics gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+
+            // Take the screenshot from the upper left corner to the right bottom corner.
+            gfxScreenshot.CopyFromScreen(screen.Bounds.X,
+                screen.Bounds.Y,
+                0,
+                0,
+                screen.Bounds.Size,
+                CopyPixelOperation.SourceCopy);
+
+            gfxScreenshot.Dispose();
+            return bmpScreenshot;
+        }
+
+        private void SetComboBoxData<T>(ComboBox comboBox, Dictionary<T, string> items)
+        {
+            comboBox.DisplayMember = "Value";
+            comboBox.ValueMember = "Key";
+            comboBox.DataSource = new BindingSource(items, null);
+        }
+
+        private void InitializeListValues()
+        {
+            SetComboBoxData(rateComboBox, new Dictionary<int, string>() {
+                { 8000, "8 kHz" },
+                { 16000, "16 kHz" },
+                { 44100, "44.1 kHz" },
+                { 48000, "48 kHz" }
+            });
+
+            Dictionary<Screen, string> screens = new Dictionary<Screen, string>();
+            foreach (Screen screen in Screen.AllScreens)
+            {
+                screens.Add(screen, String.Format("{0} [{1}x{2}]", screen.DeviceName, screen.Bounds.Width, screen.Bounds.Height));
+            }
+
+            SetComboBoxData(screenComboBox, screens);
+
+            SetComboBoxData(frameRateComboBox, new Dictionary<int, string>()
+            {
+                { 1, "1 fps"},
+                { 6, "6 fps"}
+            });
+
+            SetComboBoxData(bitRateComboBox, new Dictionary<int, string>() { 
+                { 32 * 1024, "32 kbps" },
+                { 96 * 1024, "64 kbps" },
+                { 192 * 1024, "96 kbps" }
+            });
+
+            SetComboBoxData(videoBitrateComboBox, new Dictionary<int, string>() { 
+                { 200 * 1024, "400 kbps" },
+                { 400 * 1024, "600 kbps" },
+                { 800 * 1024, "800 kbps" }
+            });
+
+        }
+
+        private void LoadDevices(DataFlow dataFlow, ComboBox combobox)
+        {
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+            MMDevice defaultDevice = (MMDevice)enumerator.GetDefaultAudioEndpoint(dataFlow, Role.Multimedia);
+
+            string defaultDeviceId;
+            defaultDevice.GetIdNative(out defaultDeviceId);
+
+            foreach (MMDevice device in enumerator.EnumAudioEndpoints(dataFlow, DeviceState.Active))
+            {
+                combobox.Items.Add(device);
+
+                string deviceId;
+                device.GetIdNative(out deviceId);
+
+                if (deviceId.Equals(defaultDeviceId))
+                {
+                    combobox.SelectedItem = device;
+                }
+            }
+        }
+
+        private void Start()
+        {
+            if (recording)
+            {
+                return;
+            }
+
+            stopwatch.Restart();
+            recordingTimer.Start();
+            frameCount = 0;
+
+            //Stream stream = new MemoryStream(1024 * 1024 * 2); //2M buffer
+
+            outputCapture = new WasapiLoopbackCapture();
+
+            outputCapture.Device = outputDeviceComboBox.SelectedItem as MMDevice;
+            outputCapture.Initialize();
+            outputWaveWriter = new WaveWriter("out.wav", outputCapture.WaveFormat);
+
+            outputCapture.DataAvailable +=
+                (s, capData) => outputWaveWriter.Write(capData.Data, capData.Offset, capData.ByteCount);
+            outputCapture.Start();
+
+
+            inputCapture = new WasapiCapture();
+            inputCapture.Device = inputDeviceComboBox.SelectedItem as MMDevice;
+            inputCapture.Initialize();
+            inputWaveWriter = new WaveWriter("in.wav", inputCapture.WaveFormat);
+
+            inputCapture.DataAvailable +=
+                (s, capData) => inputWaveWriter.Write(capData.Data, capData.Offset, capData.ByteCount);
+            inputCapture.Start();
+
+            recording = true;
+
+        }
+
+        private void Stop()
+        {
+            if (outputCapture == null)
+            {
+                return;
+            }
+
+            if (!recording)
+            {
+                return;
+            }
+
+            recordingTimer.Stop();
+            stopwatch.Restart();
+            outputCapture.Stop();
+            inputCapture.Stop();
+
+            if (outputWaveWriter != null)
+            {
+                outputWaveWriter.Dispose();
+            }
+
+            if (inputWaveWriter != null)
+            {
+                inputWaveWriter.Dispose();
+            }
+
+            outputCapture.Dispose();
+
+            inputCapture.Dispose();
+
+            recording = false;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Stop();
+        }
+    }
+}
